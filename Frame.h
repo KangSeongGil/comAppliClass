e2a43173de67bcb939ef6a4c257a8549833bf136
KangSeongGil <k.seonggil@gmail.com>
2016-11-28 21:33:31 +0900

    Frame class implement

--- Frame.h
+++ Frame.h
@@ -6,3 +6,47 @@
 #define COMAPPLICLASS_FRAME_H
 
 #endif //COMAPPLICLASS_FRAME_H
+using namespace std;
+
+class Frame {
+public:
+    int a, b;
+
+    Frame() {
+
+    }
+
+    void *operator new(size_t size) {
+        cout << "Allocation with new operation.." << endl;
+        void *temp = malloc(size);
+
+        if (!temp) {
+            throw string("bad allocation");
+        }
+
+        return temp;
+    }
+
+    void *operator new[](size_t size) {
+        cout << "Allocation with new[] operation.." << endl;
+
+        void *temp = malloc(size);
+
+        if (!temp) {
+            throw string("bad allocation");
+        }
+
+        return temp;
+    }
+
+    void operator delete(void *addr) {
+        cout << "Free With delete operator.." << endl;
+        free(addr);
+    }
+
+    void operator delete[](void *addr) {
+        cout << "Free With delete[] operator.." << endl;
+        free(addr);
+    }
+
+};
--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,18 @@
 //
 // Created by 강성길 on 2016. 11. 28..
 //
+#include <iostream>
+#include "Frame.h"
 
+int main() {
+    try {
+        Frame *f1 = new Frame;
+        Frame *f2 = new Frame[10];
+
+        delete f1;
+        delete[] f2;
+    }
+    catch (string msg) {
+        cout << "Error msg:" << msg << endl;
+    }
+}
