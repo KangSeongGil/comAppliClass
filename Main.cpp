8f3fc33eeb06fe1d2d40a3fdd657ca0e68f0ef6e
KangSeongGil <k.seonggil@gmail.com>
2016-12-04 22:25:00 +0900

    STL practice ok

--- Main.cpp
+++ Main.cpp
@@ -2,19 +2,78 @@
 // Created by 강성길 on 2016. 11. 28..
 //
 #include <iostream>
-#include "HearFile/Frame.h"
-#include "HearFile/ExArray.h"
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
+
+using namespace std;
+
+void stlVector() {
+    vector<int> v;
+
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    v.insert(v.begin(), 40);
+    v.insert(v.end(), 50);
+
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << endl;
+    }
+
+    cout << "access index at 1:" << v.at(1) << endl;
+
+    v.erase(v.begin());
+    v.erase(v.end() - 1);
+
+    vector<int>::iterator iter = v.begin();
+
+    cout << "access index at 1 with iterator:" << iter[1] << endl;
+
+    while (iter != v.end()) {
+        cout << *iter << endl;
+        iter++;
+    }
+}
+
+void dump(list<string> &l) {
+    list<string>::iterator iter = l.begin();
+
+    while (iter != l.end()) {
+        cout << *iter << endl;
+        iter++;
+    }
+}
+
+void stlList() {
+    list<string> names;
+    names.insert(names.begin(), "konkuk");
+    names.insert(names.end(), "University");
+    names.insert(names.end(), "SCLAB");
+    names.insert(names.end(), "CSY");
+    dump(names);
+    cout << "===" << endl;
+    names.reverse();
+    dump(names);
+}
+
+void stlMap() {
+    map<string, int> m;
+    m["seoul"] = 100;
+    m["daegu"] = 120;
+    m["busan"] = 200;
+
+    cout<<"Train to Seoul = " << m["seoul"]<<" $ "<<endl;
+    cout<<"Train to Daegu = " << m["Deagu"]<<" $ "<<endl;
+    cout<<"Train to Busan = " << m["Busan"]<<" $ "<<endl;
+}
 
 int main() {
-    ExArray<int> arr1(3);
-    ExArray<char> arr2(4);
-    ExArray<double> arr3(5);
-
-    arr1.addData(2);
-    arr2.addData('A');
-    arr3.addData(32.12);
-
-    arr1.pritData();
-    arr2.pritData();
-    arr3.pritData();
+    stlList();
+    cout << "\n\n\n\n";
+    stlVector();
+    cout << "\n\n\n\n";
+    stlMap();
 }
+
